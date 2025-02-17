b4a783198b024fd573ee35baa5ed81f16b4e81fd
medhavimangal08 <medhavimangal43@gmail.com>
2025-02-18 01:03:44 +0530

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [tab, showCurrentTab] = useState("Tab 1");
+  function showTab(e) {
+    showCurrentTab(e.target.innerHTML);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1 style={{ textDecoration: "underLine", fontWeight: "normal" }}>
+        Output:
+      </h1>
+      <ul>
+        <li onClick={showTab}>Tab 1</li>
+        <li onClick={showTab}>Tab 2</li>
+        <li onClick={showTab}>Tab 3</li>
+      </ul>
+      <p>This is the content for {tab}.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
